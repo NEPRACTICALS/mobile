46d3a06dd30f850ee2308d23f5532d2abffedb8a
Umutonimireille <mireilleumutoni05@gmail.com>
2024-06-24 13:04:29 +0200

    feat[removed]

--- mobile/lib/utils.js
+++ mobile/lib/utils.js
@@ -1,12 +1,4 @@
 
-export const validateEmail = (email) => {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-export const validatePassword = (password) => {
-    return password.length >= 4;
-}
 
 // function to validate post data
 
